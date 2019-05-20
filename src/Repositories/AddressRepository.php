b2e563e196a493cb9e03b4434ab46d652fd71f19
viviniko <viviniko.shop@gmail.com>
2019-05-20 12:16:53 +0800

    :trollface:

--- src/Repositories/AddressRepository.php
+++ src/Repositories/AddressRepository.php
@@ -2,54 +2,9 @@
 
 namespace Viviniko\Address\Repositories;
 
-use Viviniko\Repository\SearchRequest;
+use Viviniko\Repository\CrudRepository;
 
-interface AddressRepository
+interface AddressRepository extends CrudRepository
 {
-    /**
-     * Search.
-     *
-     * @param SearchRequest $searchRequest
-     *
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Illuminate\Database\Eloquent\Collection
-     */
-    public function search(SearchRequest $searchRequest);
 
-    /**
-     * @param $id
-     * @return mixed
-     */
-    public function find($id);
-
-    public function findBy($column, $value = null, $columns = ['*']);
-
-    /**
-     * List addresses.
-     *
-     * @param mixed $addressable
-     * @return \Illuminate\Support\Collection
-     */
-    public function lists($addressable);
-
-    /**
-     * @param array $data
-     * @return mixed
-     */
-    public function create(array $data);
-
-    /**
-     * @param $id
-     * @param array $data
-     * @return mixed
-     */
-    public function update($id, array $data);
-
-    /**
-     * Address is exists.
-     *
-     * @param $column
-     * @param null $value
-     * @return mixed
-     */
-    public function exists($column, $value = null);
 }
